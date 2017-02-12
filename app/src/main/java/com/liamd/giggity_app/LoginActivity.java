77f83d833bf05b519a95f219b8e16e64ae774e76
Woody1ooo <liam.wood@plymouth.students.ac.uk>
2017-02-12 18:22:37 +0000

    Added the logo and basic login GUI components.

--- app/src/main/java/com/liamd/giggity_app/LoginActivity.java
+++ app/src/main/java/com/liamd/giggity_app/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.liamd.giggity_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity
 {
+    EditText mEmailEditText;
+    EditText mPasswordEditText;
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmailEditText = (EditText) findViewById(R.id.emailEditText);
+        mPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
     }
 }
