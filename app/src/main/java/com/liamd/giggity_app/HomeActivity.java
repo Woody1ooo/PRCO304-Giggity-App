9b9e0d767f0a4e12712f5ef894c35bd24da61363
Woody1ooo <liam.wood@plymouth.students.ac.uk>
2017-02-13 20:52:54 +0000

    Implemented Facebook login/account creation amongst other tweaks.

--- app/src/main/java/com/liamd/giggity_app/HomeActivity.java
+++ app/src/main/java/com/liamd/giggity_app/HomeActivity.java
@@ -1,21 +1,32 @@
 package com.liamd.giggity_app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity
 {
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Test method to display the users photo from their login method
+        // Picasso could be used to make circular images...
+        Uri photoURI = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl();
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this).load(photoURI).resize(300, 300).into(imageView);
     }
 
     // Method to initialise and inflate the options menu
@@ -37,20 +48,24 @@ public class HomeActivity extends AppCompatActivity
             // is called
 
             case R.id.signOutButton:
+
+                // Will sign the user out of Gmail or email/password login
                 FirebaseAuth.getInstance().signOut();
-                StartLoginActivity();
+
+                // Will sign the user out of facebook
+                LoginManager.getInstance().logOut();
+
+                // Returns to the login activity
+
                 finish();
+                Intent returnToLoginActivity= new Intent(HomeActivity.this, LoginActivity.class);
+                returnToLoginActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(returnToLoginActivity);
+
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    // This method simply starts the login activity class
-    private void StartLoginActivity()
-    {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/liamd/giggity_app/LoginActivity.java
+++ app/src/main/java/com/liamd/giggity_app/LoginActivity.java
@@ -11,6 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -21,6 +27,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -31,6 +38,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
     private Button mLoginButton;
+    private LoginButton mFacebookLoginButton;
     private ProgressDialog mProgressDialog;
 
     // Declare Firebase specific variables
@@ -40,6 +48,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     // Declare Google specific variables
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 9001;
+
+    //Declare Facebook specific variables
+    private CallbackManager callBackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -72,6 +83,13 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         mLoginButton = (Button) findViewById(R.id.loginButton);
         mProgressDialog = new ProgressDialog(this);
 
+        // Facebook callback manager initialisation
+        callBackManager = CallbackManager.Factory.create();
+
+        // Facebook sign in button initialisation
+        mFacebookLoginButton = (LoginButton) findViewById(R.id.login_button);
+        mFacebookLoginButton.setReadPermissions("email", "public_profile");
+
         // Gmail sign in button listener
         findViewById(R.id.googleButton).setOnClickListener(this);
 
@@ -92,6 +110,30 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 SignIn();
             }
         });
+
+        // If the login is successful, this method responds to the callback and passes the access token
+        // to the handleFacebookAccessToken method
+        mFacebookLoginButton.registerCallback(callBackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel()
+            {
+
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+
+            }
+        });
+
     }
 
     @Override
@@ -193,16 +235,23 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             // If the Gmail sign in wasn't successful, a toast is displayed to that effect
             else
             {
-                Toast.makeText(LoginActivity.this, "Gmail login failed! Please ensure you have a" +
+                Toast.makeText(LoginActivity.this, "Gmail login failed! Please ensure you have a " +
                         "Gmail account associated with your android device.",
                         Toast.LENGTH_SHORT).show();
             }
         }
+
+        // Pass the activity result back to the Facebook SDK
+        callBackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     // Once the Gmail sign in has been successful, this method is called to sign in to Firebase
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct)
     {
+        // Displays the progress dialog
+        mProgressDialog.setMessage("Signing In...");
+        mProgressDialog.show();
+
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
@@ -215,6 +264,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                         // If sign in to Firebase fails, a toast displays to that effect
                         if(task.isSuccessful())
                         {
+                            mProgressDialog.hide();
                             Toast.makeText(LoginActivity.this, "Gmail sign in successful!",
                                     Toast.LENGTH_SHORT).show();
                             LoadHomeActivity();
@@ -222,7 +272,49 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
                         else
                         {
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            mProgressDialog.hide();
+                            Toast.makeText(LoginActivity.this, "Authentication failed! Please note" +
+                                    " that only one of these login methods can be used per user." +
+                                    " If you have" + "previously logged in using a different method," +
+                                    " please use this.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    // Once the Facebook sign in has been successful, this method is called to sign in to Firebase
+    private void handleFacebookAccessToken(AccessToken token)
+    {
+        // Displays the progress dialog
+        mProgressDialog.setMessage("Signing In...");
+        mProgressDialog.show();
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        // If sign in to Firebase is successful, a toast displays to that effect,
+                        // and the home activity is loaded.
+                        if(task.isSuccessful())
+                        {
+                            mProgressDialog.hide();
+                            Toast.makeText(LoginActivity.this, "Facebook sign in successful!",
+                                    Toast.LENGTH_SHORT).show();
+                            LoadHomeActivity();
+                        }
+
+                        // If sign in to Firebase fails, a toast displays to that effect
+                        else
+                        {
+                            mProgressDialog.hide();
+                            Toast.makeText(LoginActivity.this, "Authentication failed! Please note" +
+                                    " that only one of these login methods can be used per user." +
+                                    " If you have" + "previously logged in using a different method," +
+                                    " please use this.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -239,8 +331,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     // Method to load the home activity. Only called if the user is signed in.
     private void LoadHomeActivity()
     {
+        finish();
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-        finish();
     }
 }
